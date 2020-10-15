00cd5d70b4c51ce6cd61444a6cb7d017527f0a24
chahat agrawal <42664727+chahatagrawal117@users.noreply.github.com>
2020-10-15 19:36:23 +0530

    trie with unordered_map
    U can also implement trie using un_ordered_map as it is more efficient and it also saves your space complexity

--- trie.hpp
+++ trie.hpp
@@ -46,3 +46,72 @@ struct Trie{
         else return wt[root];
     }
 };
+
+
+//trie with unordered_map
+//agrawal117
+//chahatagrawal117
+#include<bits/stdc++.h>
+#define endl '\n'
+#define mod 1000000007
+typedef long long int ll;
+using namespace std;
+#define MAX 100005
+struct trie
+{
+    unordered_map<char,trie*> child;
+    bool is_end;
+    trie()
+    {
+        is_end=false;
+    }
+};
+struct trie *root;
+void insert(string s)
+{
+    struct trie *curr=root;
+    for(auto ch: s)
+    {
+        if(!(curr->child.count(ch))){
+            curr->child[ch]=new trie;
+        }
+        curr=curr->child[ch];
+    }
+    curr->is_end=true;
+}
+bool search(string s)
+{
+    struct trie *curr=root;
+    for(auto ch: s)
+    {
+        if(!(curr->child.count(ch))) return false;
+        curr=curr->child[ch];
+    }
+    return curr->is_end;
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin>>t;
+    while(t--)
+    {
+        root=new trie;
+        ll n; cin>>n;
+        for(int i=1;i<=n;i++){
+            string s; cin>>s;
+            insert(s);
+        }
+        // ll q; cin>>q;
+        ll q; q=1;
+        while(q--)
+        {
+            string s; cin>>s;
+            if(search(s)) cout<<"1"<<endl;
+            else cout<<0<<endl;
+        }
+    }
+    
+}
